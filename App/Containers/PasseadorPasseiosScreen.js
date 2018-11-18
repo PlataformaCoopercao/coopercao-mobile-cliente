606ea8fdc6c4ffc8a6623eb7581a4f6ae2b1a4ee
Bruno Simões <brunosorlandelli@gmail.com>
2018-11-18 20:16:14 -0300

    [Ajustes] Header e Footer adicionados

--- App/Containers/PasseadorPasseiosScreen.js
+++ App/Containers/PasseadorPasseiosScreen.js
@@ -3,7 +3,7 @@ import { ScrollView, KeyboardAvoidingView } from 'react-native'
 import { connect } from 'react-redux'
 import {
   Container, Header, Title, Content, Body, Text, Icon,
-  Left, Right, Accordion, Root, Button, ActionSheet, Subtitle, Card, CardItem, List
+  Left, Right, Accordion, Root, Button, ActionSheet, Subtitle, Card, CardItem, List, Footer, FooterTab, Badge
 } from 'native-base'
 import { Font, AppLoading, Expo } from "expo"
 import { Colors } from '../Themes/'
@@ -17,23 +17,6 @@ import styles from './Styles/PasseadorPasseiosScreenStyle'
 var BUTTONS = ["Iniciar Passeio", "Requisitar Substituição", "Cancelar Passeio", "Cancelar"];
 var DESTRUCTIVE_INDEX = 2;
 var CANCEL_INDEX = 3;
-var btnEditar = <Button transparent dark
-  onPress={() =>
-    ActionSheet.show(
-      {
-        options: BUTTONS,
-        cancelButtonIndex: CANCEL_INDEX,
-        destructiveButtonIndex: DESTRUCTIVE_INDEX,
-        title: "Editar Passeio"
-      },
-      buttonIndex => {
-        this.setState({ clicked: BUTTONS[buttonIndex] });
-      }
-    )}
->
-  <Icon type='Ionicons' name='ios-paw' />
-</Button>;
-
 const dataArrayPasseios = [
   'Data: 12/12/2018    Horário: 10:00\nCão: Barghest\nRua dos Bobos, nº 0',
   'Data: 12/12/2018    Horário: 10:00\nCão: Garmr\nRua dos Bobos, nº 0',
@@ -43,7 +26,7 @@ const dataArrayPasseios = [
   'Data: 12/12/2018    Horário: 10:00\nCão: Inugami\nRua dos Bobos, nº 0',
   'Data: 12/12/2018    Horário: 10:00\nCão: Anubis\nRua dos Bobos, nº 0'
 ];
-
+var tam = dataArrayPasseios.length
 class PasseadorPasseiosScreen extends Component {
   constructor(props) {
     super(props);
@@ -74,7 +57,7 @@ class PasseadorPasseiosScreen extends Component {
           <Container>
             <Header>
               <Left><Icon name='arrow-back' /></Left>
-              <Body style={{ left: -90 }}><Title>Passeios Atribuidos</Title></Body>
+              <Body><Title dark>Passeios Atribuidos</Title></Body>
             </Header>
             <Content padder>
               <ScrollView>
@@ -83,13 +66,49 @@ class PasseadorPasseiosScreen extends Component {
                     <Card>
                       <CardItem style={{justifyContent: 'space-between'}}>
                       <Text>{item}</Text>
-                        {btnEditar}
+                      {<Button transparent dark
+                        onPress={() =>
+                          ActionSheet.show(
+                            {
+                              options: BUTTONS,
+                              cancelButtonIndex: CANCEL_INDEX,
+                              title: "Passeio"
+                            },
+                            buttonIndex => {
+                              this.setState({ clicked: BUTTONS[buttonIndex] });
+                            }
+                          )}
+                      >
+                        <Icon type='Ionicons' name='ios-paw' />
+                      </Button>}
                       </CardItem>
                     </Card>
                   }>
                 </List>
               </ScrollView>
             </Content>
+              <Footer>
+                <FooterTab>
+                  <Button>
+                    <Icon name='person'/>
+                    <Text>Perfil</Text>
+                  </Button>
+                  <Button>
+                    <Icon name='md-calendar'/>
+                    <Text>Histórico</Text>
+                  </Button>
+                  <Button badge vertical>
+                    <Badge info><Text>2</Text></Badge>
+                    <Icon name='list-box'/>
+                    <Text>Atribuido</Text>
+                  </Button>
+                  <Button badge vertical>
+                    <Badge info><Text>5</Text></Badge>
+                    <Icon name='walk'/>
+                    <Text>Livre</Text>
+                  </Button>
+                </FooterTab>
+              </Footer>
           </Container>
         </Root>
       )
