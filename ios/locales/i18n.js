5a8bc281d1b261c7e589afaa215af4b409e49526
Eduardo Roque dos Santos <duduroque08@hotmail.com>
2018-11-22 01:14:02 -0200

    Development Update
    Adding i18n.js in locales

---
+++ ios/locales/i18n.js
@@ -0,0 +1,30 @@
+import ReactNative from 'react-native';
+import I18n from 'react-native-i18n';
+
+// Import all locales
+import en from './en.json';
+import pt from './pt.json';
+
+// Should the app fallback to English if user locale doesn't exists
+I18n.fallbacks = true;
+
+// Define the supported translations
+I18n.translations = {
+  en,
+  pt
+};
+
+const currentLocale = I18n.currentLocale();
+
+// Is it a RTL language?
+export const isRTL = currentLocale.indexOf('pt') === 0 || currentLocale.indexOf('ar') === 0;
+
+// Allow RTL alignment in RTL languages
+ReactNative.I18nManager.allowRTL(isRTL);
+
+// The method we'll use instead of a regular string
+export function strings(name, params = {}) {
+  return I18n.t(name, params);
+};
+
+export default I18n;
