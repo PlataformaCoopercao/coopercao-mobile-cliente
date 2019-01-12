0216ea5447fd39efa904d9e4212a381a15b86f4c
Rodrigo <ropi2203@gmail.com>
2019-01-12 20:38:53 -0200

    integração Cadastrar Cachorro

--- App/Containers/CadastroDogScreen.js
+++ App/Containers/CadastroDogScreen.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react'
 import { ScrollView, KeyboardAvoidingView } from 'react-native'
 import { connect } from 'react-redux'
-import {Container, Header, Title, Content, Body, Text, Icon,
+import { Alert } from 'react-native'
+import {
+  Container, Header, Title, Content, Body, Text, Icon,
   Left, Right, Accordion, Root, Button, ActionSheet, Subtitle, Card,
-   CardItem, List, Footer, FooterTab, Badge, Form, Item, Label, Input,
-   Picker, Spinner, Thumbnail, Col, Grid, Row, ListItem, InputGroup
+  CardItem, List, Footer, FooterTab, Badge, Form, Item, Label, Input,
+  Picker, Spinner, Thumbnail, Col, Grid, Row, ListItem, InputGroup
 } from 'native-base'
 import { Font, AppLoading, Expo } from "expo"
 import { StackNavigator } from "react-navigation"
 // Add Actions - replace 'Your' with whatever your reducer is called :)
 // import YourActions from '../Redux/YourRedux'
 import { strings } from '../locales/i18n';
+import axios from 'axios';
+import * as firebase from 'firebase';
 // Styles
 import { Images, Colors } from '../Themes';
 import { TextInput } from 'react-native-gesture-handler';
@@ -22,22 +26,37 @@ class CadastroDogScreen extends Component {
     super(props);
     this.state = {
       fontLoading: true, // to load font in expo
+      age: '0',
+      gender: '0',
+      habits: '0',
+      interaction_dogs: '0',
+      interaction_external: '0',
+      interaction_people: '0',
+      name: '0',
+      obs: '0',
+      owner: '0',
+      photoUrl: '0',
+      port: 'key0',
+      race: '0',
+      vet_name: '0',
+      vet_phone: '0',
       clicked: '',
       edited: '',
       selectedItem: undefined,
       selected: 'key0',
       results: {
-          items: [],
+        items: [],
       },
     };
   }
-  onValueChange(value: string) {
-    this.setState({
-      selected: value
-    });
-  }
 
-  
+  //onValueChange(value: string) {
+  //  this.setState({
+  //    selected: value
+  //  });
+  //}
+
+
   // required to load native-base font in expo
   async componentWillMount() {
     await Font.loadAsync({
@@ -48,73 +67,108 @@ class CadastroDogScreen extends Component {
     this.setState({ fontLoading: false });
   }
 
-  render () {
-    const {navigate} = this.props.navigation;
+  onCadastrarPress = () => {
+
+    this.submit();
+  }
+//MUITOS PARAMETROS EM FALTA!
+  submit() {
+    let dog = {}
+    dog.age = this.state.age,
+      dog.gender = 'M',
+      dog.habits = 'Normal',
+      dog.interaction_dogs = 'normal',
+      dog.interaction_external = 'normal',
+      dog.interaction_people = 'normal',
+      dog.name = this.state.name,
+      dog.obs = 'normal',
+      dog.owner = firebase.auth().currentUser.uid,
+      dog.photoUrl = 'https://i.pinimg.com/originals/8c/24/46/8c2446109522a9d244197544d92fe210.jpg',
+      dog.port = this.state.port,
+      dog.race = this.state.race,
+      dog.vet_name = 'João Carlos',
+      dog.vet_phone = '+558166666666';
+    var url = 'https://us-central1-coopercao-backend.cloudfunctions.net/addDog';
+    axios.post(url, dog)
+      .then(() => {
+        Alert.alert('Cão cadastrado com sucesso!');
+        this.props.navigation.navigate('MeusCachorrosScreen');
+      })
+      .catch((error) => {
+        Alert.alert(error.message);
+      });
+  }
+
+  render() {
+    const { navigate } = this.props.navigation;
     const uri = "https://static1.squarespace.com/static/573b62e9746fb941c1458dcd/t/58bf1f27d1758e5d0c580379/1488921550603/who-we-are.jpg";
     if (this.state.fontLoading) {
       return (
-      <Container>
-        <Header />
-        <Content>
-          <Spinner color='red' />
-        </Content>
-      </Container>
+        <Container>
+          <Header />
+          <Content>
+            <Spinner color='red' />
+          </Content>
+        </Container>
       );
     } else {
-    return (
-      <Container style={{backgroundColor:'red'}}>
-          <Header style={{backgroundColor:'red', marginTop: 22}}>
-          <Left>
-            <Icon name='arrow-back' style={{width: 40}} onPress={() => navigate('MeusCachorrosScreen')}/>
-          </Left>
-          <Body>
-            <Title style={{ left: -90, color: Colors.snow }}>{strings('CadastroDogScreen.dogRegister')}</Title>
-          </Body>
-        </Header>
-        <Content padder style={{backgroundColor: 'white', alignContent:"stretch"}}>
-        <List>
-          <ListItem style={{alignSelf:'center'}}>
-            <Thumbnail large source={{uri: uri}}/>
-          </ListItem>
-          <ListItem>
-              <InputGroup>
-                  <Input placeholder={strings('CadastroDogScreen.placeHName')} />
-              </InputGroup>
-          </ListItem> 
-          <ListItem>
-              <InputGroup>
-                  <Input placeholder={strings('CadastroDogScreen.placeHAge')} />
-              </InputGroup>
-          </ListItem>
-          <ListItem>
-              <InputGroup>
-                  <Input placeholder={strings('CadastroDogScreen.placeHRace')}/>
-              </InputGroup>
-          </ListItem>
-          <ListItem>
-              <Text>{strings('CadastroDogScreen.size')}</Text>
-              <Picker
-                iosHeader={strings('CadastroDogScreen.chooseOne')}
-                mode="dropdown"
-                selectedValue={this.state.selected}
-                onValueChange={this.onValueChange.bind(this)} >
+      return (
+        <Container style={{ backgroundColor: 'red' }}>
+          <Header style={{ backgroundColor: 'red', marginTop: 22 }}>
+            <Left>
+              <Icon name='arrow-back' style={{ width: 40 }} onPress={() => navigate('MeusCachorrosScreen')} />
+            </Left>
+            <Body>
+              <Title style={{ left: -90, color: Colors.snow }}>{strings('CadastroDogScreen.dogRegister')}</Title>
+            </Body>
+          </Header>
+          <Content padder style={{ backgroundColor: 'white', alignContent: "stretch" }}>
+            <List>
+              <ListItem style={{ alignSelf: 'center' }}>
+                <Thumbnail large source={{ uri: uri }} />
+              </ListItem>
+              <ListItem>
+                <InputGroup>
+                  <Input placeholder={strings('CadastroDogScreen.placeHName')} onChangeText={(text) => { this.setState({ name: text }) }} />
+                </InputGroup>
+              </ListItem>
+              <ListItem>
+                <InputGroup>
+                  <Input placeholder={strings('CadastroDogScreen.placeHAge')} onChangeText={(text) => { this.setState({ age: text }) }} />
+                </InputGroup>
+              </ListItem>
+              <ListItem>
+                <InputGroup>
+                  <Input placeholder={strings('CadastroDogScreen.placeHRace')} onChangeText={(text) => { this.setState({ race: text }) }} />
+                </InputGroup>
+              </ListItem>
+              <ListItem>
+                <Text>{strings('CadastroDogScreen.size')}</Text>
+                <Picker
+                  iosHeader={strings('CadastroDogScreen.chooseOne')}
+                  mode="dropdown"
+                  selectedValue={this.state.port}
+                  onValueChange={(value) => {
+                    //this.onValueChange.bind(this);
+                    this.setState({ port: value });
+                    }} >
                   <Item label={strings('CadastroDogScreen.mini')} value="key0" />
                   <Item label={strings('CadastroDogScreen.little')} value="key1" />
                   <Item label={strings('CadastroDogScreen.medium')} value="key2" />
                   <Item label={strings('CadastroDogScreen.big')} value="key3" />
                   <Item label={strings('CadastroDogScreen.giant')} value="key4" />
-              </Picker>
-          </ListItem>
-      </List>
-      <Button style={{ alignSelf: 'center', marginTop: 20, marginBottom: 20, backgroundColor:'red' }} 
-      onPress={() => navigate('MeusCachorrosScreen')}>
-      <Text>{strings('CadastroDogScreen.register')}</Text>   
-      </Button>
-        </Content>
-        <Footer style={{ backgroundColor: 'red' }}>
+                </Picker>
+              </ListItem>
+            </List>
+            <Button style={{ alignSelf: 'center', marginTop: 20, marginBottom: 20, backgroundColor: 'red' }}
+              onPress={this.onCadastrarPress}>
+              <Text>{strings('CadastroDogScreen.register')}</Text>
+            </Button>
+          </Content>
+          <Footer style={{ backgroundColor: 'red' }}>
             <FooterTab style={{ backgroundColor: 'red' }}>
               <Button onPress={() => navigate('PerfilClienteScreen')}>
-              <Icon name='md-person' type='Ionicons' style={{color:'white'}}/>
+                <Icon name='md-person' type='Ionicons' style={{ color: 'white' }} />
                 <Text style={{ color: 'white' }}>{strings('Footer.profile_button')}</Text>
               </Button>
               <Button onPress={() => navigate('HistoricoClienteScreen')}>
@@ -131,8 +185,8 @@ class CadastroDogScreen extends Component {
               </Button>
             </FooterTab>
           </Footer>
-      </Container>
-    )
+        </Container>
+      )
     }
   }
 }
